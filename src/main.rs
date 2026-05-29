7e0912a46745a4cbd179514552153133d4608265
msujas <kenneth.marshall@esrf.fr>
2026-05-29 16:36:27 +0200

    make avdir

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,7 @@ fn main() {
     let e1 = now.elapsed();
     println!("loading cbfs and ponis took {} s", e1.as_secs());
     let avdir = format!("{cbfdir}/{subdir}");
+    let _ = create_dir(&avdir);
     mf.average_cakes(4., &cakedir, &avdir, cakemaskfile);
     let elapsed =  now.elapsed();
     println!("");
